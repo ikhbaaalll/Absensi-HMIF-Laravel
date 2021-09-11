a62120ae7667db9852e8a387723090bbba9bb82f
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-09-12 01:37:39 +0700

    add filter report index view

--- resources/views/dashboard/report/index.blade.php
+++ resources/views/dashboard/report/index.blade.php
@@ -1,50 +1,78 @@
 @extends('master.dashboard')
 
 @section('title-page')
-Daftar Kegiatan
+    Daftar Kegiatan
 @endsection
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/table/dataTables.bootstrap4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/table/responsive.bootstrap4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/table/buttons.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Daftar Kegiatan</h6>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="example1" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>Judul</th>
-                            <th>Tempat</th>
-                            <th>Kegiatan</th>
-                            <th>Waktu</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($kegiatans as $kegiatan)
-                        <tr>
-                            <td>{{$kegiatan->judul}}</td>
-                            <td>{{$kegiatan->tempat}}</td>
-                            <td>{{$kegiatan->kegiatan}}</td>
-                            <td>{{$kegiatan->waktu->format('d-m-Y H:i')}}</td>
-                            <td>
-                                <a href="{{ route('kegiatan.show', $kegiatan) }}"
-                                    class="btn btn-sm btn-primary">Presensi</a>
-                            </td>
-                        </tr>
-                        @empty($kegiatans)
-                        <tr>
-                            <td colspan="4">Tidak ada data kegiatan</td>
-                        </tr>
-                        @endempty
+    <div class="row">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Daftar Kegiatan</h6>
+            </div>
+            <div class="card-body">
+                <div class="my-3">
+                    <form method="GET" action="{{ route('kegiatan.index') }}">
+                        <select name="kategori" class="form-control col-2 d-inline-block">
+                            <option value="">Pilih Kategori</option>
+                            @forelse ($kategoris as $kategori)
+                                <option value="{{ $kategori->kegiatan }}"
+                                    {{ $kategori->kegiatan == request()->get('kategori') ? 'selected' : '' }}>
+                                    {{ $kategori->kegiatan }}
+                                </option>
+                            @empty
+                                <option value="" disabled>Tidak ada kategori</option>
+                            @endforelse
+                        </select>
+                        <select name="tempat" class="form-control col-2 d-inline-block">
+                            <option value="">Pilih Tempat</option>
+                            @forelse ($tempats as $tempat)
+                                <option value="{{ $tempat->tempat }}"
+                                    {{ $tempat->tempat == request()->get('tempat') ? 'selected' : '' }}>
+                                    {{ $tempat->tempat }}</option>
+                            @empty
+                                <option value="" disabled>Tidak ada tempat</option>
+                            @endforelse
+                        </select>
+                        <button type="submit" class="btn btn-primary">Filter</button>
+                    </form>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="example1" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Judul</th>
+                                <th>Tempat</th>
+                                <th>Kegiatan</th>
+                                <th>Waktu</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($kegiatans as $kegiatan)
+                                <tr>
+                                    <td>{{ $kegiatan->judul }}</td>
+                                    <td>{{ $kegiatan->tempat }}</td>
+                                    <td>{{ $kegiatan->kegiatan }}</td>
+                                    <td>{{ $kegiatan->waktu->format('d-m-Y H:i') }}</td>
+                                    <td>
+                                        <a href="{{ route('kegiatan.show', $kegiatan) }}"
+                                            class="btn btn-sm btn-primary">Presensi</a>
+                                        <a href="{{ route('kegiatan.edit', $kegiatan) }}"
+                                            class="btn btn-sm btn-warning">Ubah</a>
+                                    </td>
+                                </tr>
+                            @empty($kegiatans)
+                                <tr>
+                                    <td colspan="4">Tidak ada data kegiatan</td>
+                                </tr>
+                            @endempty
                         @endforeach
                     </tbody>
                 </table>
@@ -68,16 +96,17 @@ Daftar Kegiatan
 <script src="{{ asset('js/table/buttons.print.min.js') }}"></script>
 <script src="{{ asset('js/table/buttons.colVis.min.js') }}"></script>
 <script>
-    $(function() {  
-    $("#example1").DataTable({
-      "responsive": true,
-      "lengthChange": false,
-      "autoWidth": false,
-      "buttons": ['copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5', "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-  });
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+            "buttons": ['copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5', "colvis"
+            ]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
 </script>
 @endsection
