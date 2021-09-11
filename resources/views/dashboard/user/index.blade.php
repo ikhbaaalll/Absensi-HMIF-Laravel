35e0979fe35464a907ebd8b6408b181fdb90ffd2
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-09-12 01:39:05 +0700

    update user index

--- resources/views/dashboard/user/index.blade.php
+++ resources/views/dashboard/user/index.blade.php
@@ -1,61 +1,59 @@
 @extends('master.dashboard')
 
 @section('title-page')
-Daftar Calon Anggota
+    Daftar Calon Anggota
 @endsection
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/table/dataTables.bootstrap4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/table/responsive.bootstrap4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/table/buttons.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Daftar User</h6>
-        </div>
-        <div class="card-body">
-            @if (session('status'))
-            <div class="alert alert-success mt-3">
-                {{ session('status') }}
+    <div class="row">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Daftar User</h6>
             </div>
-            @endif
-            <div class="my-3">
-                <a href="{{route('user.create')}}" class="btn btn-primary">Tambah User</a>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-bordered" id="example1" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>Nama</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($users as $user)
-                        <tr>
-                            <td>{{$user->name}}</td>
-                            <td>
-                                <a href="{{ route('user.edit', $user) }}"
-                                    class="btn btn-sm btn-warning">Edit</a>
-                                <form class="d-inline"
-                                    action="{{route('user.destroy', $user)}}"
-                                    method="POST">
-                                    @csrf
-                                    @method("DELETE")
-                                    <button type="submit"
-                                        onclick="return confirm('Yakin ingin menghapus {{$user->name}}')"
-                                        class="btn btn-sm btn-danger d-inline-block">Hapus</button>
-                                </form>
-                            </td>
-                        </tr>
-                        @empty($users)
-                        <tr>
-                            <td colspan="2">Tidak ada data kegiatan</td>
-                        </tr>
-                        @endempty
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success mt-3">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <div class="my-3">
+                    <a href="{{ route('user.create') }}" class="btn btn-primary">Tambah User</a>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="example1" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Nama</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($users as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td>
+                                        <a href="{{ route('user.edit', $user) }}" class="btn btn-sm btn-warning">Edit</a>
+                                        <form class="d-inline" action="{{ route('user.destroy', $user) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method("DELETE")
+                                            <button type="submit"
+                                                onclick="return confirm('Yakin ingin menghapus {{ $user->name }}')"
+                                                class="btn btn-sm btn-danger d-inline-block">Hapus</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @empty($users)
+                                <tr>
+                                    <td colspan="2">Tidak ada data kegiatan</td>
+                                </tr>
+                            @endempty
                         @endforeach
                     </tbody>
                 </table>
@@ -79,16 +77,17 @@ Daftar Calon Anggota
 <script src="{{ asset('js/table/buttons.print.min.js') }}"></script>
 <script src="{{ asset('js/table/buttons.colVis.min.js') }}"></script>
 <script>
-    $(function() {  
-    $("#example1").DataTable({
-      "responsive": true,
-      "lengthChange": false,
-      "autoWidth": false,
-      "buttons": ['copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5', "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-  });
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+            "buttons": ['copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5', "colvis"
+            ]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
 </script>
 @endsection
