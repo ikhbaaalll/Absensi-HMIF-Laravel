8af4be4b87969b19200296ffa34d8c7b1b1b519c
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-09-12 01:35:02 +0700

    update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,61 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Arithmatic 3.0 Kaderisasi">
+    <meta name="author" content="Muhammad Ikhbal">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <link rel="icon" href="{{ asset('icon/icon_round.png') }}" type="image/x-icon">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Login Arithmatic 3.0</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('vendor/login/login.css') }}">
+</head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+<body>
+    <div class="container-fluid">
+        <div class="row main-content text-center">
+            <div class="col-md-5 text-center company__info">
+                <span class="company__logo m-4">
+                    <img src="{{ asset('icon/hmif.png') }}" alt="" class="img img-thumbnail" width="400px"
+                        height="400px" style="background-color: #06122e">
+                </span>
+                <h5 class="company_title">Himpunan Mahasiswa Informatika</h5>
+            </div>
+            <div class="col-md-7 col-xs-12 col-sm-12 login_form ">
+                <div class="container-fluid">
+                    <div class="row mt-4">
+                        <h2>Log In</h2>
+                    </div>
+                    <div class="row">
+                        <form action="{{ route('login') }}" method="POST" class="form-group">
+                            @csrf
+                            <div class="row">
+                                <input type="email" name="email" id="E-mail" class="form__input"
+                                    placeholder="E-mail">
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            <div class="row">
+                                <!-- <span class="fa fa-lock"></span> -->
+                                <input type="password" name="password" id="password" class="form__input"
+                                    placeholder="Password">
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                            <div class="row justify-content-center">
+                                <input type="submit" value="Login" class="btn">
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    <!-- Footer -->
+    {{-- <div class="container-fluid text-center footer">
+        Coded with &hearts; by <a href="https://bit.ly/yinkaenoch">Yinka.</a></p>
+    </div> --}}
+</body>
+
+</html>
