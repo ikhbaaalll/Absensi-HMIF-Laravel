062954a232b9b686de57f365e258652442f9e984
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-09-12 01:31:07 +0700

    update ReportController

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -3,30 +3,57 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\KegiatanRequest;
 use App\Models\Absen;
+use App\Models\CalonAnggota;
 use App\Models\Kegiatan;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $kegiatans = Kegiatan::latest()->get();
+        $kegiatans = Kegiatan::when($request->filled('kategori'), function ($query) {
+            $query->where('kegiatan', request()->get('kategori'));
+        })
+            ->when($request->filled('tempat'), function ($query) {
+                $query->where('tempat', request()->get('tempat'));
+            })
+            ->latest()
+            ->get();
 
-        return view('dashboard.report.index', compact('kegiatans'));
+        $kategoris = Kegiatan::select('kegiatan')->distinct()->get();
+
+        $tempats = Kegiatan::select('tempat')->distinct()->get();
+
+        return view('dashboard.report.index', compact('kegiatans', 'kategoris', 'tempats'));
     }
 
-    public function show(Kegiatan $kegiatan)
+    public function show(Request $request, Kegiatan $kegiatan)
     {
-        $kegiatan = $kegiatan->load('absen', 'absen.calonAnggota');
+        $kelompoks = CalonAnggota::select('kelompok')->distinct()->orderBy('kelompok')->get();
 
-        return view('dashboard.report.show', compact('kegiatan'));
+        $absens = CalonAnggota::with(['absen' => function ($query) use ($kegiatan) {
+            $query->where('kegiatan_id', $kegiatan->id);
+        }])
+            ->when(request()->filled('kelompok'), function ($query) {
+                $query->where('kelompok', request()->get('kelompok'));
+            })
+            ->orderBy('kelompok')
+            ->get();
+
+        return view('dashboard.report.show', compact('kegiatan', 'kelompoks', 'absens'));
     }
 
-    public function destroy(Request $request, $id)
+    public function edit(Kegiatan $kegiatan)
     {
-        Absen::find($id)->delete();
+        return view('dashboard.report.edit', compact('kegiatan'));
+    }
 
-        return redirect()->route('kegiatan.show', $request->kegiatan)->with('status', 'Sukses menghapus presensi');
+    public function update(KegiatanRequest $request, Kegiatan $kegiatan)
+    {
+        $kegiatan->update($request->validated());
+
+        return redirect()->route('kegiatan.index')->with('status', "Berhasil mengubah kegiatan {$kegiatan->judul}");
     }
 }
