8257b6e4985ac9c7779ee0d7eb8b691f970fec8c
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-09-12 01:36:45 +0700

    update index calon anggota

--- resources/views/dashboard/calonanggota/index.blade.php
+++ resources/views/dashboard/calonanggota/index.blade.php
@@ -1,67 +1,67 @@
 @extends('master.dashboard')
 
 @section('title-page')
-Daftar Calon Anggota
+    Daftar Calon Anggota
 @endsection
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/table/dataTables.bootstrap4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/table/responsive.bootstrap4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/table/buttons.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Daftar Calon Anggota</h6>
-        </div>
-        <div class="card-body">
-            @if (session('status'))
-            <div class="alert alert-success mt-3">
-                {{ session('status') }}
+    <div class="row">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Daftar Calon Anggota</h6>
             </div>
-            @endif
-            <div class="my-3">
-                <a href="{{route('calonanggota.create')}}" class="btn btn-primary">Tambah Calon Anggota</a>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-bordered" id="example1" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>Nama</th>
-                            <th>NIM</th>
-                            <th>Prodi</th>
-                            <th>QR Code</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($calonAnggotas as $calonAnggota)
-                        <tr>
-                            <td>{{$calonAnggota->nama}}</td>
-                            <td>{{$calonAnggota->nim}}</td>
-                            <td>{{$calonAnggota->prodi}}</td>
-                            <td><a href="{{$calonAnggota->qr_code}}" download>Lihat QR</a></td>
-                            <td>
-                                <a href="{{ route('calonanggota.edit', ['calonanggotum' => $calonAnggota]) }}"
-                                    class="btn btn-sm btn-warning">Edit</a>
-                                <form class="d-inline"
-                                    action="{{route('calonanggota.destroy', ['calonanggotum' => $calonAnggota])}}"
-                                    method="POST">
-                                    @csrf
-                                    @method("DELETE")
-                                    <button type="submit"
-                                        onclick="return confirm('Yakin ingin menghapus {{$calonAnggota->nama}}')"
-                                        class="btn btn-sm btn-danger d-inline-block">Hapus</button>
-                                </form>
-                            </td>
-                        </tr>
-                        @empty($calonAnggotas)
-                        <tr>
-                            <td colspan="5">Tidak ada data kegiatan</td>
-                        </tr>
-                        @endempty
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success mt-3">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <div class="my-3">
+                    <a href="{{ route('calonanggota.create') }}" class="btn btn-primary">Tambah Peserta</a>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="example1" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Nama</th>
+                                <th>NIM</th>
+                                <th>Kelompok</th>
+                                <th>QR Code</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($calonAnggotas as $calonAnggota)
+                                <tr>
+                                    <td>{{ $calonAnggota->nama }}</td>
+                                    <td>{{ $calonAnggota->nim }}</td>
+                                    <td>{{ $calonAnggota->kelompok }}</td>
+                                    <td><a href="{{ $calonAnggota->qr_code }}" download>Lihat QR Code</a></td>
+                                    <td>
+                                        <a href="{{ route('calonanggota.edit', ['calonanggotum' => $calonAnggota]) }}"
+                                            class="btn btn-sm btn-warning">Edit</a>
+                                        <form class="d-inline"
+                                            action="{{ route('calonanggota.destroy', ['calonanggotum' => $calonAnggota]) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method("DELETE")
+                                            <button type="submit"
+                                                onclick="return confirm('Yakin ingin menghapus {{ $calonAnggota->nama }}')"
+                                                class="btn btn-sm btn-danger d-inline-block">Hapus</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @empty($calonAnggotas)
+                                <tr>
+                                    <td colspan="5">Tidak ada data kegiatan</td>
+                                </tr>
+                            @endempty
                         @endforeach
                     </tbody>
                 </table>
@@ -85,16 +85,18 @@ Daftar Calon Anggota
 <script src="{{ asset('js/table/buttons.print.min.js') }}"></script>
 <script src="{{ asset('js/table/buttons.colVis.min.js') }}"></script>
 <script>
-    $(function() {  
-    $("#example1").DataTable({
-      "responsive": true,
-      "lengthChange": false,
-      "autoWidth": false,
-      "buttons": ['copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5', "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-  });
+    $(function() {
+        $("#example1").DataTable({
+            "order": [],
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+            "buttons": ['copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5', "colvis"
+            ]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
 </script>
 @endsection
